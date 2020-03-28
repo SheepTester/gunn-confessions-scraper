2ecbfd312ef2b750468ea848837987c8f6d833b1
SheepTester <ussa.say@gmail.com>
2020-03-28 16:29:15 -0700

    Get the NEXT set of confessions :O

--- scraper.py
+++ scraper.py
@@ -1,5 +1,17 @@
 from pyquery import PyQuery
-from lxml import html
 import requests
+import json
 
-print(PyQuery('https://www.facebook.com/pg/gunnconfessions/posts/').find('._3576').text())
+facebook_base = 'https://www.facebook.com'
+
+page = PyQuery(facebook_base + '/pg/gunnconfessions/posts/')
+for confession in page.find('._3576').items():
+    # print('#1: ' + confession.text())
+    pass
+
+see_more = page.find('#www_pages_reaction_see_more_unitwww_pages_posts a[ajaxify]').attr('ajaxify')
+# For some reason `__a=1` is necessary
+response = requests.get(facebook_base + see_more + '&__a=1')
+see_more = PyQuery(json.loads(response.content[9:])['domops'][0][3]['__html'])
+for confession in see_more.find('._3576').items():
+    print('#2: ' + confession.text())
