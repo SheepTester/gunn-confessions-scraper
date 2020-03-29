e32a737ec977ac73f31364ab33244376c42af99c
SheepTester <ussa.say@gmail.com>
2020-03-28 17:57:23 -0700

    get post info is pretty crazy

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from pyquery import PyQuery
 import requests
 import json
+import re
 
 # Class key
 # ._3576 - a <div>, the parent of each <p> in a post
@@ -18,23 +19,28 @@ def searchJSON(matches, match_fn, json):
         for val in json.values():
             searchJSON(matches, match_fn, val)
 
+get_num = r'/gunnconfessions/posts/(\d+)'
+
 page = PyQuery(facebook_base + '/pg/gunnconfessions/posts/')
 epic_script_tag = page.find('script').filter(lambda i, this: PyQuery(this).text().startswith('new (require("ServerJS"))()'))
-# print('\n\n'.join([x.text()[0:30] for x in page.find('script').items()]))
 post_info = json.loads(epic_script_tag.text()[35:-2])
 is_feedback = lambda json: isinstance(json, dict) and 'feedback' in json
 feedback_things = []
 searchJSON(feedback_things, is_feedback, post_info)
+post_id_to_feedback = {}
 for feedback_thing in feedback_things:
-    print(feedback_thing['feedback']['share_fbid'])
+    id = feedback_thing['feedback']['share_fbid']
+    post_id_to_feedback[id] = feedback_thing['feedback']
 for confession in page.find('._3576').items():
     # print('#1: ' + confession.text())
     post_link = confession.parent().find('span > a._5pcq').attr('href')
+    post_id = re.match(get_num, post_link).group(1)
     # page = PyQuery(facebook_base + post_link)
     # print(facebook_base + post_link)
-    # with open('./dumb-fb.html', 'w', encoding='utf-8') as f:
-    #     f.write(page.html(method='html'))
-    print(post_link)
+    # print(post_id + ' ' + str(post_id in post_id_to_feedback))
+    with open('./dumb-fb.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(post_id_to_feedback[post_id], indent=2))
+    break
 
 # see_more = page.find('#www_pages_reaction_see_more_unitwww_pages_posts a[ajaxify]').attr('ajaxify')
 # # For some reason `__a=1` is necessary
