98dcfb4c37e451752757a5e0635588f7e791fff2
SheepTester <ussa.say@gmail.com>
2021-03-21 21:58:38 -0700

    Support for fetching more than 3 (requires cookies :( ) and other things - Read cookie values from .env - Use cookie for getting > 3 (max 297, apparently) - No longer crashes if someone posts a sticker - No longer crashes when you reach the end - Support for non-GC pages - Get creation time and URL of posts

--- attempt4/get_page.py
+++ attempt4/get_page.py
@@ -1,6 +1,7 @@
-from urllib.parse import urlencode
-import json
-import requests
+import os;
+from urllib.parse import urlencode, quote;
+import json;
+import requests;
 
 class Reaction:
     def __init__(self, raw_reaction):
@@ -15,7 +16,7 @@ class Comment:
         raw_comment = raw_comment['node'];
         self.author = raw_comment['author']['name'];
         self.author_url = raw_comment['author']['url'];
-        self.content = raw_comment['body']['text'];
+        self.content = raw_comment['body']['text'] if raw_comment['body'] is not None else '';
         # *Seconds* since epoch
         self.created = raw_comment['created_time'];
         # There's also `edit_history`
@@ -40,50 +41,82 @@ class Post:
             Comment(comment)
             for comment in feedback['display_comments']['edges']
         ];
+        self.created = sections['context_layout']['story']['comet_sections']['timestamp']['story']['creation_time'];
+        self.url = sections['context_layout']['story']['comet_sections']['timestamp']['story']['url'];
 
     def __repr__(self):
         return f'{repr(self.post)} comments={self.comment_count} reactions={self.reaction_count} ({" ".join(map(repr, self.reactions))}) shares={self.share_count}' \
             + ''.join('\n' + repr(comment) for comment in self.comments);
 
-def get_page(cursor, count=3):
-    body = urlencode({
-        # No idea what this means, but it's needed
-        'fb_dtsg': 'AQHS7sAiDqyR:AQHryv1x5_m1',
+class PageGetter:
+    def __init__(self, page_id, doc_id):
+        self.cookie = f'c_user={os.getenv("COOKIE_C_USER")}; xs={quote(os.getenv("COOKIE_XS"))}';
+        self.page_id = page_id;
+        self.doc_id = doc_id;
 
-        # Presumably this refers to the Gunn Confessions page
-        'doc_id': '4130381023661578',
+    def get_page(self, cursor=None, count=3):
+        '''
+        Fetches posts from the Facebook page with cookies as listed in `.env`.
 
-        'variables': json.dumps({
-            'count': count,
-            'cursor': cursor,
+        :param cursor: A string containing the next cursor to get the next set
+        of posts, or `None` to get the first page.
+        :param count: The number of posts to get. The maximum is 297, for some
+        reason.
+        :returns: A tuple with a list of `Post`s and the cursor for the next
+        page.
+        ''';
 
-            # Needed for comments to be listed
-            'feedLocation': 'PAGE_TIMELINE',
+        body = urlencode({
+            # No idea what this means, but it's needed
+            'fb_dtsg': 'AQHS7sAiDqyR:AQHryv1x5_m1',
 
-            # If omitted, Facebook gives a warning about it being omitted
-            'privacySelectorRenderLocation': 'COMET_STREAM',
+            # GraphQL document ID, apparently
+            'doc_id': self.doc_id,
 
-            # Not sure what this is an ID for
-            'id': '1792991304081448',
-        }),
-    });
-    response = requests.post(
-        'https://www.facebook.com/api/graphql/',
-        data=body,
-        headers={
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    ).json();
-    posts = [
-        Post(post)
-        for post in response['data']['node']['timeline_feed_units']['edges']
-    ];
-    next_cursor = response['data']['node']['timeline_feed_units']['page_info']['end_cursor'];
-    return posts, next_cursor;
+            'variables': json.dumps({
+                'count': count,
+                'cursor': cursor,
+
+                # Needed for comments to be listed
+                'feedLocation': 'PAGE_TIMELINE',
+
+                # If omitted, Facebook gives a warning about it being omitted
+                'privacySelectorRenderLocation': 'COMET_STREAM',
+
+                # Gunn Confessions page ID
+                'id': self.page_id,
+            }),
+        });
+        response = requests.post(
+            'https://www.facebook.com/api/graphql/',
+            data=body,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': self.cookie,
+            },
+        ).json();
+        posts = [
+            Post(post)
+            for post in response['data']['node']['timeline_feed_units']['edges']
+            # The earliest post (or a pfp change) may have message: null
+            if isinstance(post['node']['comet_sections']['content']['story']['comet_sections']['message'], dict)
+        ];
+        next_cursor = response['data']['node']['timeline_feed_units']['page_info']['end_cursor'];
+        return posts, next_cursor;
 
 if __name__ == '__main__':
-    posts, next_cursor = get_page('AQHRL2j5XiIzM7fAzUHHJ7DQ_MItv0J99qXmJIqBrPrMaiQha8EXzktfiJww3vsXwYvRurRrTFwN7p4od0nVgZD27en9hNJ8JVxkF_xjm6ml1yIkBZlbrY1FKSj1xwyCfhII');
+    from dotenv import load_dotenv;
+    load_dotenv();
+
+    # Gunn Confessions
+    # page_getter = PageGetter('1792991304081448', '4130381023661578');
+
+    # Test confessions page
+    page_getter = PageGetter('634679850309851', '4130381023661578');
+
+    posts, next_cursor = page_getter.get_page();
     print('\n\n'.join(map(repr, posts)));
-    posts, next_cursor = get_page(next_cursor);
-    print('\n\n'.join(map(repr, posts)));
+    posts, next_cursor = page_getter.get_page(cursor=next_cursor, count=50);
+    print(len(posts));
+    # print('\n\n'.join(map(repr, posts)));
     print(next_cursor);
