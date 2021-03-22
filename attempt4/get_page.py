2566dfc452bfe5e76622528e52c0e91d40c2d283
SheepTester <ussa.say@gmail.com>
2021-03-21 22:41:39 -0700

    Serialize as YAML Will have to scrape tomorrow

--- attempt4/get_page.py
+++ attempt4/get_page.py
@@ -1,4 +1,5 @@
 import os;
+from datetime import datetime;
 from urllib.parse import urlencode, quote;
 import json;
 import requests;
@@ -24,12 +25,19 @@ class Comment:
     def __repr__(self):
         return f'{repr(self.content)} - {self.author}'
 
+    def yaml(self, indent=''):
+        yaml = indent + f'- author: {self.author}\n';
+        yaml += indent + f'  content: {json.dumps(self.content, ensure_ascii=False)}\n';
+        yaml += indent + f'  created: {self.created}\n';
+        yaml += indent + f'  author-url: {self.author_url}\n';
+        return yaml;
+
 class Post:
     def __init__(self, raw_post):
         sections = raw_post['node']['comet_sections'];
         feedback = sections['feedback']['story']['feedback_context']['feedback_target_with_context'];
 
-        self.post = sections['content']['story']['comet_sections']['message']['story']['message']['text'];
+        self.content = sections['content']['story']['comet_sections']['message']['story']['message']['text'];
         self.comment_count = feedback['comment_count']['total_count'];
         self.reaction_count = feedback['comet_ufi_summary_and_actions_renderer']['feedback']['reaction_count']['count'];
         self.share_count = feedback['comet_ufi_summary_and_actions_renderer']['feedback']['share_count']['count'];
@@ -45,8 +53,27 @@ class Post:
         self.url = sections['context_layout']['story']['comet_sections']['timestamp']['story']['url'];
 
     def __repr__(self):
-        return f'{repr(self.post)} comments={self.comment_count} reactions={self.reaction_count} ({" ".join(map(repr, self.reactions))}) shares={self.share_count}' \
+        return f'{repr(self.content)} comments={self.comment_count} reactions={self.reaction_count} ({" ".join(map(repr, self.reactions))}) shares={self.share_count}' \
             + ''.join('\n' + repr(comment) for comment in self.comments);
+
+    def yaml(self, indent=''):
+        yaml = indent + f'- content: {json.dumps(self.content, ensure_ascii=False)}\n';
+        reactions = {};
+        for reaction in self.reactions:
+            reactions[reaction.type] = reaction.count;
+        # I'm too used to Standard.JS now
+        reactions = '{ %s }' % json.dumps(reactions)[1:-1] if len(self.reactions) > 0 else '{}';
+        yaml += indent + f'  reactions: {reactions}\n';
+        yaml += indent + f'  comments:';
+        if len(self.comments) > 0:
+            yaml += '\n';
+            for comment in self.comments:
+                yaml += comment.yaml(indent=indent + '  ');
+        else:
+            yaml += ' []\n';
+        yaml += indent + f'  created: {self.created}\n';
+        yaml += indent + f'  url: {self.url}\n';
+        return yaml;
 
 class PageGetter:
     def __init__(self, page_id, doc_id):
@@ -63,7 +90,7 @@ class PageGetter:
         :param count: The number of posts to get. The maximum is 297, for some
         reason.
         :returns: A tuple with a list of `Post`s and the cursor for the next
-        page.
+        page, which may be None if you're on the last page.
         ''';
 
         body = urlencode({
@@ -101,7 +128,8 @@ class PageGetter:
             # The earliest post (or a pfp change) may have message: null
             if isinstance(post['node']['comet_sections']['content']['story']['comet_sections']['message'], dict)
         ];
-        next_cursor = response['data']['node']['timeline_feed_units']['page_info']['end_cursor'];
+        page_info = response['data']['node']['timeline_feed_units']['page_info'];
+        next_cursor = page_info['end_cursor'] if page_info['has_next_page'] else None;
         return posts, next_cursor;
 
 if __name__ == '__main__':
@@ -114,9 +142,14 @@ if __name__ == '__main__':
     # Test confessions page
     page_getter = PageGetter('634679850309851', '4130381023661578');
 
-    posts, next_cursor = page_getter.get_page();
-    print('\n\n'.join(map(repr, posts)));
-    posts, next_cursor = page_getter.get_page(cursor=next_cursor, count=50);
-    print(len(posts));
-    # print('\n\n'.join(map(repr, posts)));
-    print(next_cursor);
+    time = datetime.now().strftime('%Y-%m-%d_%H.%M.%S');
+    with open(f'./output/a4_{time}.yml', 'a', encoding='utf8') as file:
+        cursor = None;
+        while True:
+            posts, cursor = page_getter.get_page(cursor, count=297);
+            file.write('\n'.join(post.yaml() for post in posts));
+            if cursor is None:
+                break;
+            else:
+                print(cursor);
+        print('Done! :D');
